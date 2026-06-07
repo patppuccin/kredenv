5b00fcf46c25ade0d7f88aa17786a369d523c755
Patrick Ambrose (patppuccin) <patrickambrosericky@gmail.com>
2026-06-07 18:16:30 +0530

    chore: move args check to PreRun & remove unused commented code

--- src/cmd/hook.go
+++ src/cmd/hook.go
@@ -26,19 +26,19 @@ var hookCmd = &cobra.Command{
 	GroupID:       "setup",
 	SilenceUsage:  true,
 	SilenceErrors: true,
-	Run: func(cmd *cobra.Command, args []string) {
+	PreRun: func(cmd *cobra.Command, args []string) {
 		if len(args) != 1 {
 			termactions.Log().Error("Expected a shell name as the only argument")
 			os.Exit(1)
 		}
-
+	},
+	Run: func(cmd *cobra.Command, args []string) {
 		sh, ok := hooks.Get(args[0])
 		if !ok {
 			termactions.Log().Error("The shell '" + args[0] + "' is not supported")
 			termactions.Log().Info("Supported shells are " + hooks.Names())
 			os.Exit(1)
 		}
-		// fmt.Println(sh.Hook)
 		fmt.Print(strings.ReplaceAll(sh.Hook, "\r\n", "\n"))
 	},
 }
